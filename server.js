b03ead02e763232b614b6df3b8784b56ab7d2a48
dydaev <dydaev@gmail.com>
2017-08-10 01:57:41 +0300

    config express server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 80;
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/build'));
+
+// set the home page route
+app.get('/public/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('tratata');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
