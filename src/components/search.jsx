9c2a1508e767ce4d549d04877ef933ae7b4c18b0
dydaev <dydaev@gmail.com>
2017-08-09 14:37:59 +0300

    change component Searche to PureComponent

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 
-const Search = props =>{
-  const changeLocation = () => {
+class Search extends PureComponent
+{
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      location:  "",
+      country: ""
+    }
+  }
+  componentWillUpdate(nextProps, nextState) {
+    this.setState({
+      location:  nextProps.weather.city === undefined ? "" : nextProps.weather.city.name,
+      country: nextProps.weather.city === undefined ? "" : nextProps.weather.city.country
+    })
+  }
+  changeLocation() {
     if(this.locationInput.value !== "") {
-      props.focus(this.locationInput.value);
-      this.locationInput.value = ""
+      this.props.focus(this.locationInput.value);
+      this.locationInput.value = "";
     }
   }
-  const changeVal = event => {
+  changeVal(event) {
     if(event.key === "Enter") {
-      changeLocation();
+      this.changeLocation();
     }
   }
-  let location = props.weather.city === undefined ? "" : props.weather.city.name;
-  let country = props.weather.city === undefined ? "" : props.weather.city.country;
-  return (
-    <div className="conteiner-search">
-      <input 
-        id="search"
-        ref={(input) => {
-          this.locationInput = input
-        }} 
-        onBlur={changeLocation} 
-        onKeyPress={changeVal}
-        className="search" 
-        type="search" 
-        results="5"
-        placeholder={location}
-      />
-        <span>{country}</span>
-    </div>
-  )
+  render() {
+    console.log("search: Rendered ", this.state.location, ":", this.state.country)
+    return(
+      <div className="conteiner-search">
+        <input 
+          id="search"
+          ref={ input => {
+            this.locationInput = input
+          }} 
+          onBlur={this.changeLocation.bind(this)} 
+          onKeyPress={this.changeVal.bind(this)}
+          className="search" 
+          type="search" 
+          results="5"
+          placeholder={this.state.location}
+        />
+        <span>{this.state.country}</span>
+      </div>
+    )
+  }
 }
 
 export default Search;
