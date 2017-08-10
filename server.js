d6a7254ff961c32cf9cbdd3d7c16bf6ccb88913a
romaChe <dydaev@gmail.com>
2017-08-10 08:20:46 +0300

    Update server.js
    config server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var port = process.env.PORT || 80;
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/build/static'));
+app.use(express.static(__dirname + '/build'));
 
 // set the home page route
-app.get('/build', function(req, res) {
+app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
     res.render('tratata');
